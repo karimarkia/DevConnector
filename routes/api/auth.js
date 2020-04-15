0a403012695e687fe9be641a32ef9a70179f9542
Karim Arkia <karimarkia@gmail.com>
2020-04-15 11:49:41 +0300

    backend

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const auth = require('../../middleware/auth')
-
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const { check, validationResult } = require('express-validator')
 const User = require('../../models/User')
 
 router.get('/', auth, async (req, res) => {
@@ -12,5 +15,61 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server Error')
   }
 })
+
+router.post(
+  '/',
+  [
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter a valid password').isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const error = validationResult(req)
+    if (!error.isEmpty()) {
+      return res.status(400).json({ error: error.array() })
+    }
+
+    const { email, password } = req.body
+    console.log(password);
+    
+    try {
+      // Check if user exist
+      let user = await User.findOne({ email })
+      console.log('user : ' , user);
+      
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: [{ msg: 'Invalid Email or Password' }] })
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password)
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ error: [{ msg: 'Invalid Email or Password' }] })
+      }
+
+      //   return JWT
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      }
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err
+          res.json({ token })
+        }
+      )
+    } catch (err) {
+      console.error(err.massage)
+      res.status(500).send('Server Error')
+    }
+  }
+)
 
 module.exports = router
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,103 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator')
+const auth = require('../../middleware/auth')
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
 
-router.get('/', (req, res) => res.send('Profile Router'))
+// @route    GET api/profile/me
+// @desc     Get current users profile
+// @access   Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['name', 'avatar'])
+    if (!profile) {
+      return res
+        .status(400)
+        .json({ msg: 'There is no profile for this user !' })
+    }
+
+    res.json(profile)
+  } catch (error) {
+    console.error(error.massage)
+    res.status(500).send('Server Error')
+  }
+})
+
+// @route    POST api/profile
+// @desc     Create or update user profile
+// @access   Private
+router.post(
+  '/',
+  auth,
+  [
+    check('status', 'Status is required').not().isEmpty(),
+    check('skills', 'Skills is required').not().isEmpty(),
+  ],
+  async (req, res) => {
+    const error = validationResult(req)
+    if (!error.isEmpty()) {
+      return res.status(400).json({ error: error.array() })
+    }
+
+    const {
+      company,
+      location,
+      website,
+      bio,
+      skills,
+      status,
+      githubusername,
+      youtube,
+      twitter,
+      instagram,
+      linkedin,
+      facebook,
+    } = req.body
+
+    const profileFields = {}
+    profileFields.user = req.user.id
+    if (company) profileFields.company = company
+    if (website) profileFields.website = website
+    if (location) profileFields.location = location
+    if (bio) profileFields.bio = bio
+    if (githubusername) profileFields.githubusername = githubusername
+    if (status) profileFields.status = status
+    if (skills) {
+      profileFields.skills = skills.split(',').map((skill) => skill.trim())
+    }
+
+    profileFields.social = {}
+    if (youtube) profileFields.social.youtube = youtube
+    if (twitter) profileFields.social.twitter = twitter
+    if (instagram) profileFields.social.instagram = instagram
+    if (linkedin) profileFields.social.linkedin = linkedin
+    if (facebook) profileFields.social.facebook = facebook
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id })
+      if (profile) {
+        // Updated
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        )
+        return res.json(profile)
+      }
+      // create new one
+      profile = new Profile(profileFields)
+
+      await profile.save()
+      res.json(profile)
+
+    } catch (error) {
+      console.log(error.massage)
+      res.send(500).json('Server error')
+    }
+  }
+)
 
 module.exports = router
