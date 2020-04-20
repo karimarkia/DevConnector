2b7b070319d205c2495c59d5bc02612dce0c88c5
Karim Arkia <karimarkia@gmail.com>
2020-04-20 21:19:45 +0300

    starting with the frontend

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Navbar from './component/Layout/Navbar'
+import Landing from './component/Layout/Landing'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Landing />
+    </div>
+  )
+}
+
+export default App
