558e58fb66f75c1cfa88f465cf53be9d5cbb6155
Karim Arkia <karimarkia@gmail.com>
2020-04-29 00:26:21 +0300

    set up redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import './App.css'
+import { Route, Switch } from 'react-router-dom'
 import Navbar from './component/Layout/Navbar'
 import Landing from './component/Layout/Landing'
+import Login from './component/auth/Login'
+import Register from './component/auth/Register'
 
 const App = () => {
   return (
-    <div>
+    <>
       <Navbar />
-      <Landing />
-    </div>
+      <Route path="/" exact component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Switch>
+      </section>
+    </>
   )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import Store from './Store/store'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={Store}>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </Provider>,
   document.getElementById('root')
-);
-
+)
