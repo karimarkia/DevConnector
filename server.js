d45759d6f55fb1afcea6e4eab6b7852d0e2632ff
karim arkia <karimarkia@gmailcom>
2020-07-05 17:03:47 +0300

    prep for deploy

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const users = require('./routes/api/users')
 const posts = require('./routes/api/posts')
 const auth = require('./routes/api/auth')
 const profile = require('./routes/api/profile')
+const path = require('path')
 
 // DB connected
 connectDB()
@@ -18,6 +19,18 @@ app.use('/api/users', users)
 app.use('/api/posts', posts)
 app.use('/api/auth', auth)
 app.use('/api/profile', profile)
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server Run At ${PORT}`))
